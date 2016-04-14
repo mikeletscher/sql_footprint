e5ff993b758d16c568afc9cb9a9ceeeec7507a66
Brandon Joyce <bjoyce@covermymeds.com>
2016-04-14 14:29:17 -0400

    Better regex

--- lib/sql_footprint/sql_anonymizer.rb
+++ lib/sql_footprint/sql_anonymizer.rb
@@ -1,14 +1,10 @@
 module SqlFootprint
   class SqlAnonymizer
-    gsubs = {}
-    ['>', '<', '!=', '<=', '>='].each do |operator|
-      gsubs[/\s#{Regexp.quote(operator)}\s([0-9]+)/] = " #{operator} number-redacted"
-    end
     GSUBS = {
       /\sIN\s\((.*)\)/ => ' IN (values-redacted)'.freeze, # IN clauses
-      /\s\=\s([0-9]+)/ => ' = number-redacted'.freeze, # numbers
       /\s'(.*)\'/ => " 'value-redacted'".freeze, # literal strings
-    }.merge(gsubs).freeze
+      /\s+(!=|=|<|>|<=|>=)\s+[0-9]+/ => ' \1 number-redacted', # numbers
+    }.freeze
 
     def anonymize sql
       GSUBS.reduce(sql) do |s, (regex, replacement)|
