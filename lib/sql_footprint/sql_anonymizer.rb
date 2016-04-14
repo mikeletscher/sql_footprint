da018b9601c3f8082f4ef07a4f3f880a6fa89811
Brandon Joyce <bjoyce@covermymeds.com>
2016-04-14 09:27:13 -0400

    Rubocop fix

--- lib/sql_footprint/sql_anonymizer.rb
+++ lib/sql_footprint/sql_anonymizer.rb
@@ -1,14 +1,14 @@
 module SqlFootprint
   class SqlAnonymizer
+    gsubs = {}
+    ['>', '<', '!=', '<=', '>='].each do |operator|
+      gsubs[/\s#{Regexp.quote(operator)}\s([0-9]+)/] = " #{operator} number-redacted"
+    end
     GSUBS = {
       /\sIN\s\((.*)\)/ => ' IN (values-redacted)'.freeze, # IN clauses
       /\s\=\s([0-9]+)/ => ' = number-redacted'.freeze, # numbers
       /\s'(.*)\'/ => " 'value-redacted'".freeze, # literal strings
-    }
-    ['>', '<', '!=', '<=', '>='].each do |operator|
-      GSUBS[/\s#{Regexp.quote(operator)}\s([0-9]+)/] = " #{operator} number-redacted"
-    end
-    GSUBS.freeze
+    }.merge(gsubs).freeze
 
     def anonymize sql
       GSUBS.reduce(sql) do |s, (regex, replacement)|
